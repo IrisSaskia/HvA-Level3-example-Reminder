870283a7093fb96f02101e08c4536176bf7b3bb5
IrisSaskia <iris.oostra@hva.nl>
2020-03-20 21:18:57 +0100

    Will try again tomorrow

--- Reminder level 3/app/src/main/java/com/example/reminder/AddActivity.kt
+++ Reminder level 3/app/src/main/java/com/example/reminder/AddActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_add.*
 
-class Main2Activity : AppCompatActivity() {
+class AddActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
